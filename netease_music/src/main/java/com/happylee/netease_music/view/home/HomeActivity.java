e15891cbd35da86d5993943ad6679a5bdda4c7e5
lijunde <lijunde@didiglobal.com>
2020-12-16 22:54:16 +0800

    关闭了androidx,删除了所有模块中的androidTest文件夹(这些文件夹中有androidx内容),导入了一些drawable和mipmap图片资源,让appcompat和support库版本相同

---
+++ netease_music/src/main/java/com/happylee/netease_music/view/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.happylee.netease_music.view.home;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.happylee.netease_music.R;
+
+public class HomeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
